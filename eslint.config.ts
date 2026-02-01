332506a45c1221b17483ff805131542aaa838a97
Florian Imdahl <git@ffflorian.de>
2026-02-01 13:51:44 +0100

    chore: Disable sourceMap, fix ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
 import config from '@ffflorian/eslint-config';
-import {defineConfig} from 'eslint/config';
+import {Config, defineConfig} from 'eslint/config';
 
-export default defineConfig([config, {rules: {'no-magic-numbers': 'off'}}]);
+export default defineConfig([config as Config, {rules: {'no-magic-numbers': 'off'}}]);
