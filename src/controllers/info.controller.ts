c539a1aefc4a9ca1768cf93ce41540ea5032065a
Florian Imdahl <git@ffflorian.de>
2026-06-06 12:07:35 +0200

    fix: De-duplicate version loading

--- src/controllers/info.controller.ts
+++ src/controllers/info.controller.ts
@@ -1,27 +1,14 @@
 import {Controller, Get} from '@nestjs/common';
 import {ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
-import {findUp} from 'find-up';
 import {StatusCodes as HTTP_STATUS} from 'http-status-codes';
-import {promises as fs} from 'node:fs';
 
+import {config} from '../config.js';
 import {InfoResult} from '../swagger.js';
-import {getLogger} from '../utils.js';
 
 interface InfoRouteResponseBody {
   code: HTTP_STATUS;
   commit?: string;
   version?: string;
-}
-
-const logger = getLogger('controllers/InfoController');
-
-let version: string | undefined;
-
-const packageJsonPath = await findUp('package.json', {allowSymlinks: false, cwd: '.', type: 'file'});
-if (!packageJsonPath) {
-  logger.warn('Could not find file `package.json`. Version will not be shown in the `_info` endpoint');
-} else {
-  version = JSON.parse(await fs.readFile(packageJsonPath, 'utf-8')).version;
 }
 
 @ApiTags('Server Info')
@@ -34,7 +21,7 @@ export class InfoController {
     return {
       code: HTTP_STATUS.OK,
       commit: process.env.SOURCE_COMMIT || 'main',
-      ...(version && {version}),
+      version: config.VERSION,
     };
   }
 }
